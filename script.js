279f2dc1055110c3ea4de41a6ff3b3f230284168
ahmed2916 <ahmedkhandelhi123@gmail.com>
2024-01-16 01:35:07 +0530

    created a new section and some design and some javascript code

--- script.js
+++ script.js
@@ -2,12 +2,17 @@
 //authentication token eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YTVkMzc0ZWFlOWM4MGY0YjNlYjE0MGRiNzVmZmNmZiIsInN1YiI6IjY1OTg0NTBmMWQxYmY0MDIwMjNjODBjYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.G1VrqvBo8M6JtJLAag_J5c4vy9oHp4n-QWZOn_bR0WQ'
 
 window.onload = (event) => {
-  pullData();
+  pullDataTrending();
+  pullTrailerData();
 };
 
 let ImageArr={
 
-  src1:"./images/aqua.jpg",
+  src1:"./images/img1.jpg",
+
+  src2:"./images/img2.jpg",
+
+  src3:"./images/img3.jpg",
 
 
 
@@ -16,6 +21,89 @@ let ImageArr={
 
 
 
+
+function pullTrailerData()
+{
+let i=1;
+  for(const property in ImageArr)
+  {
+     let trailer=document.getElementById("Trailer");
+  let card=document.createElement("div");
+  let childDiv=document.createElement("div");
+  let img=document.createElement("img");
+
+
+  // let img=document.createElement("img");
+  childDiv.className="childCont";
+  img.className="playButton";
+  card.class="trailerCard";
+ 
+  // img.class="TrailercardImage  ";
+  card.id=`A${+i}`;
+  card.className="imageContainer"
+
+  img.src=`${"./images/play.png"}`;
+  card.style.backgroundImage = `url(${ImageArr[property]})`; 
+
+  // SVG.style.backgroundImage = `url(${"./images/playbtn"})`; 
+
+  // card.appendChild(img);
+  childDiv.appendChild(img);
+  card.appendChild(childDiv);
+  trailer.appendChild(card);
+  i++;
+
+
+  }
+// console.log(i);
+
+let Element1 = document.getElementById("A1");
+let Element2 = document.getElementById("A2");
+let Element3 = document.getElementById("A3");
+  Element1.addEventListener(
+    "mouseover",
+    (event) => {
+      // highlight the mouseover target
+      let trailer=document.getElementById("Trailer");
+
+      // trailer.style.backgroundColor = "green";
+      trailer.style.backgroundImage = `url(${ImageArr.src1})`; 
+  
+    
+    },
+  
+  );
+
+  Element2.addEventListener(
+    "mouseover",
+    (event) => {
+      // highlight the mouseover target
+      let trailer=document.getElementById("Trailer");
+      trailer.style.backgroundColor = "grey";
+      trailer.style.backgroundImage = `url(${ImageArr.src2})`; 
+      trailer.style.backgroundImage="opacity(0.5)";
+      // trailer.style.background=  `linear-gradient(  #d7d7d7, rgba(0, 0, 0, 0.7), url(${ImageArr.src2}))`;
+  
+    
+    },
+  
+  );
+
+  Element3.addEventListener(
+    "mouseover",
+    (event) => {
+      // highlight the mouseover target
+      let trailer=document.getElementById("Trailer");
+      // let idImg=document.getElementById("trailerimg");
+       trailer.style.backgroundImage = `url(${ImageArr.src3})`; 
+  
+    
+    },
+  
+  );
+
+
+}
 
 //keyword Search Result Function
 function matchString(val,title)
@@ -103,7 +191,7 @@ function Trending(response)
 
 
 
-function pullData(event)
+function pullDataTrending(event)
 {
 
   // const options = {
